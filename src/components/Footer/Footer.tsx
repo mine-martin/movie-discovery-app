1da2df4d4a2939bf95cbc90a5b98170690c9e91d
martin wangondu <wangondumn@gmail.com>
2023-09-15 23:59:42 +0300

    Mobile view

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -10,8 +10,8 @@ import {
 
 const Footer = () => {
   return (
-    <section className="mt-10 pt-10 pb-10">
-      <div className="flex items-center justify-center flex-col gap-10">
+    <section className="mt-10 pt-10 pb-10 max-sm:mt-5">
+      <div className="flex items-center justify-center flex-col gap-10 max-sm:gap-6">
         <div className="text-4xl flex flex-row gap-12">
           <Link href="/">
             <AiFillFacebook />
@@ -26,7 +26,7 @@ const Footer = () => {
             <AiFillYoutube />
           </Link>
         </div>
-        <div className="flex text-gray-900 items-center gap-12 font-bold text-[18px]">
+        <div className="flex text-gray-900 items-center gap-12 max-sm:gap-6 font-bold text-[18px]">
           <Link href="/">
             <p className=" "> Condition of Use</p>
           </Link>
@@ -37,11 +37,13 @@ const Footer = () => {
             <p className="">Press Room</p>
           </Link>
         </div>
-        <div className="flex items-center gap-2 text-gray-500 font-bold ">
+        <div className="flex items-center gap-2 text-gray-500 font-bold max-sm:flex-col-reverse">
           <span className="text-[18px]">
             <AiOutlineCopyrightCircle />
           </span>
-          <p className="max-lg:text-sm">2021 MovieBox by MARTIN WANGONDU</p>
+          <p className="max-lg:text-sm max-sm:text-xs ">
+            2021 MovieBox by MARTIN WANGONDU
+          </p>
         </div>
       </div>
     </section>
